2e2c9c0aaf899bcbd8095239ac3204b7d3953b37
ajgutierrezr <ajgutierrezr@gmail.com>
2018-05-10 00:03:13 -0300

    info seed

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'nio4r', '~> 2.3.0'
+gem 'roo', '~> 2.7.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'roo'
+  xlsx = Roo::Spreadsheet.open('./public/Grupos-1.xlsx')
+  xlsx.default_sheet = 'Formulas'
+  (2..70).each do |m|
+    ProductInfo.create(
+    sku: xlsx.cell(m,1),
+    product: xlsx.cell(m,2),
+    lote: xlsx.cell(m,3),
+    nro: xlsx.cell(m,4),
+    man: xlsx.cell(m,5),
+    nar: xlsx.cell(m,6),
+    fru: xlsx.cell(m,7),
+    fra: xlsx.cell(m,8),
+    dur: xlsx.cell(m,9),
+    ara: xlsx.cell(m,10))
+  end
